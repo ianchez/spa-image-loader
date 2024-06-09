5b045a9fe9068c973f61ec4e1d528d631b6b824b
Seba Sanchez <20444043+ianchez@users.noreply.github.com>
2024-06-09 18:35:44 +0200

    chore: added Go Home button on Item Screen

--- src/app/screens/ItemScreen.tsx
+++ src/app/screens/ItemScreen.tsx
@@ -1,8 +1,16 @@
+import { useNavigate, useParams } from "react-router-dom";
 
 function ItemScreen() {
+  const navigate = useNavigate();
+  const { id } = useParams();
+
   return (
     <section>
-      <h1>Item Screen</h1>
+      <h1>Item {id}</h1>
+
+      <button onClick={() => navigate("/")}>
+        Go Home
+      </button>
     </section>
   );
 }
