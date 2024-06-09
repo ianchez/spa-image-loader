77b401a44d63079703f43e21e8bd8855b7780618
Seba Sanchez <20444043+ianchez@users.noreply.github.com>
2024-06-09 19:49:48 +0200

    chore: render images in Home Screen

--- src/app/screens/HomeScreen.tsx
+++ src/app/screens/HomeScreen.tsx
@@ -1,13 +1,35 @@
 import { useAppSelector } from '../../app/hooks';
 import { selectAllPhotos } from '../../features/fetchAll/fetchAllSlice';
 
+import './index.css';
+
+const FALLBACK_IMAGE = 'https://www.lighting.philips.com.sg/content/dam/b2b-philips-lighting/ecat-fallback.png?wid=93&hei=93&qlt=82';
+
 function HomeScreen() {
-  const data = useAppSelector(selectAllPhotos);
-  console.log(data);
+  const allPhotos = useAppSelector(selectAllPhotos);
+
+  console.log({ allPhotos })
 
   return (
     <section>
       <h1>Home Screen</h1>
+
+      <div id="photos-container">
+        {allPhotos.map((photoItem) => (
+          <div
+            key={photoItem.index}
+            className="photo-item"
+          >
+            <a href={`/${photoItem.index}/item`} >
+              <img
+                src={photoItem.image}
+                alt={photoItem.title}
+                onError={(e) => e.currentTarget.src = FALLBACK_IMAGE}
+              />
+            </a>
+          </div>
+        ))}
+      </div>
     </section>
   );
 }
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,20 +1,9 @@
 import { PhotoItem } from "../types";
 
-export const isValidUrl = (url: any): boolean => {
-  if (!url) return false;
-  if (typeof url !== 'string') return false;
-
-  try {
-    const parsedUrl = new URL(url);
-    return ['http:', 'https:'].includes(parsedUrl.protocol);
-  } catch (e) {
-    return false;
-  }
-};
 
 export const parsePhotoItem = (item: any): PhotoItem => ({
   title: String(item?.title) || '',
   description: String(item?.description) || '',
-  image: isValidUrl(item.image) ? item.image : undefined,
+  image: String(item.image) ?? '',
   index: parseInt(item?.index || 0),
 });
